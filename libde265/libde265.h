bebe70b61f5b95e27ab95f8fd0add2ece64d37a2
mihuaz2Z <mihuayouxi@163.com>
2017-06-02 09:24:55 +0800

    Initial Commit

---
+++ libde265/libde265.h
@@ -0,0 +1,13 @@
+//
+//  libde265.h
+//  libde265
+//
+//  Created by Ruiwen Feng on 2017/6/2.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libde265 : NSObject
+
+@end
---
+++ libde265/libde265.m
@@ -0,0 +1,13 @@
+//
+//  libde265.m
+//  libde265
+//
+//  Created by Ruiwen Feng on 2017/6/2.
+//  Copyright © 2017年 Ruiwen Feng. All rights reserved.
+//
+
+#import "libde265.h"
+
+@implementation libde265
+
+@end
